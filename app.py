dad3f66a169879e9097803de0e5bb030b82947cf
Priscilla <17732979+priscillaoclark@users.noreply.github.com>
2024-09-17 14:14:29 +0000

    add returnUser function

--- app.py
+++ app.py
@@ -12,9 +12,27 @@ def addUser():
     return render_template('form.html')
 
 # return user
-@app.route('/getUser', methods=['POST', 'GET'])
+@app.route('/returnUser', methods=['POST', 'GET'])
 def getUser():
-    
+    # inspect posted data
+   inspect = {}
+   inspect['args'] = request.args
+   inspect['form'] = request.form
+   inspect['values'] = request.values
+   inspect['first'] = request.form.get('first')
+   inspect['last'] = request.form.get('last')
+   inspect['path'] = request.path
+   inspect['url'] = request.url
+   inspect['base_url'] = request.base_url
+   inspect['content_encoding'] = request.content_encoding
+   inspect['content_length'] = request.content_length
+   inspect['content_type'] = request.content_type
+   inspect['host'] = request.host
+   inspect['mimetype'] = request.mimetype
+   if (request.method == 'POST' and
+       'application/json' in request.headers.get('Content-Type')):
+       inspect['receivedJson'] = request.get_json()
+   return jsonify(inspect)
 
 # start server
 if __name__ == '__main__':
